4481ec75486af7f56f4739f188021bff1ebce1a7
mauriciio89 <maugalvez89@gmail.com>
2022-10-13 03:03:52 +0000

    avance

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User,Characters, Planets, FavoritesCharacters, FavoritesPlanets
 #from models import Person
 
 app = Flask(__name__)
@@ -32,12 +32,15 @@ def sitemap():
 
 @app.route('/user', methods=['GET'])
 def handle_hello():
-
+    users = User.query.filter().all()
+    result = list(map(lambda user: user.serialize(), users))
     response_body = {
         "msg": "Hello, this is your GET /user response "
     }
 
     return jsonify(response_body), 200
+
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -6,6 +6,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
+    name = db.Column(db.String(120), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
@@ -15,5 +16,103 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+
+    # do not serialize the password, its a security breach
+    }
+
+    
+
+class Characters(db.Model):
+    __tablename__ ='characters'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = db.Column(db.Integer, primary_key=True)
+    character_name = db.Column(db.String(250))
+    character_height = db.Column(db.String(250))
+    character_hair_color = db.Column(db.String(250), nullable=False)
+    character_skin_color = db.Column(db.String(250), nullable=False)
+    character_eyes_color = db.Column(db.String(250), nullable=False)
+    character_gender = db.Column(db.String(250), nullable=False)
+
+    def __repr__(self):
+        return '<Characters %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "height": self.character_height,
+            "hair_color": self.hair_color,
+            "skin_color": self.skin_color,
+            "eyes_color": self.eyes_color,
+            "gender": self.gender
+
+    # do not serialize the password, its a security breach
+    }
+
+   
+
+class Planets(db.Model):
+    __tablename__ ='planets'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = db.Column(db.Integer, primary_key=True)
+    planet_population = db.Column(db.Integer, primary_key=True)
+    planet_diameter = db.Column(db.Integer, primary_key=True)
+    planet_climate = db.Column(db.String(250), nullable=False)
+    planet_gravity = db.Column(db.String(250), nullable=False)
+
+    def __repr__(self):
+        return '<Planets %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "planet_population": self.name,
+            "planet_diameter": self.character_height,
+            "planet_climate": self.hair_color,
+            "planet_gravity": self.skin_color
+            
+    }
+
+class FavoritesCharacters(db.Model):
+    __tablename__ = 'favoritescharacters'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    character_id = db.Column (db.Integer, db.ForeignKey('characters.id'))
+
+    def __repr__(self):
+        return '<Planets %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "character_id": self.character_id
+            
+    }
+
+    
+
+class FavoritesPlanets(db.Model):
+    __tablename__ = 'favoritesplanets'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column (db.Integer, db.ForeignKey('user.id'))
+    planet_id = db.Column (db.Integer, db.ForeignKey('planets.id'))
+
+    def __repr__(self):
+        return '<Planets %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "planet_id": self.planet_id
+            
+    }    
+
+   
